f49fcb76430469b86a2c34e46c4741b603c72eb4
Debanjan <debanjankanjilal9@gmail.com>
2023-04-01 01:32:51 +0530

    event handling and EventEmitter

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -1,5 +1,4 @@
-import {  } from "@angular/core";
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Todo } from "src/app/Todo";
 
 @Component({
@@ -9,10 +8,16 @@ import { Todo } from "src/app/Todo";
 })
 export class TodoItemComponent implements OnInit{
   @Input() todo: Todo;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+
   constructor() {
      
   }
 
   ngOnInit(): void {    
+  }
+  onClick(todo : Todo){
+    this.todoDelete.emit(todo);
+    console.log("onClick has been triggered")
   }
 }
